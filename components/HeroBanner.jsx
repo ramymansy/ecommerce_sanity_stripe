77abd3c570fa6b463014e84d1ee570983683c429
Ramy-Mansy <rami_manse@outlook.com>
2022-05-01 07:04:19 +0200

    edit links

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -3,26 +3,36 @@ import Link from "next/link";
 
 import { urlFor } from "../lib/client";
 
-const HeroBanner = ({ heroBanner }) => {
+const HeroBanner = ({
+  heroBanner: {
+    product,
+    smallText,
+    midText,
+    largeText1,
+    image,
+    desc,
+    buttonText,
+  },
+}) => {
   return (
     <div className="hero-banner-container">
       <div>
-        <p className="beats-solo">{heroBanner.smallText}</p>
-        <h3>{heroBanner.midText}</h3>
-        <h1>{heroBanner.largeText1}</h1>
+        <p className="beats-solo">{smallText}</p>
+        <h3>{midText}</h3>
+        <h1>{largeText1}</h1>
         <img
-          src={urlFor(heroBanner.image)}
+          src={urlFor(image)}
           alt="headphones"
           className="hero-banner-image"
         />
 
         <div>
-          <Link href={`/product/${heroBanner.product}`}>
-            <button type="button">{heroBanner.buttonText}</button>
-          </Link>
+          {/*<Link href={`/product/${product}`}>
+            <button type="button">{buttonText}</button>
+  </Link>*/}
           <div className="desc">
             <h5>Description</h5>
-            <p>{heroBanner.desc}</p>
+            <p>{desc}</p>
           </div>
         </div>
       </div>
